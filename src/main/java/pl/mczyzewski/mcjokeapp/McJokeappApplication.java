2f2d7fb1189ee379b24b705d464dc8338431b6c9
Michal Czyzewski <czy.mich@gmail.com>
2019-10-02 00:34:20 +0200

    Initial commit

---
+++ src/main/java/pl/mczyzewski/mcjokeapp/McJokeappApplication.java
@@ -0,0 +1,13 @@
+package pl.mczyzewski.mcjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McJokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McJokeappApplication.class, args);
+    }
+
+}
