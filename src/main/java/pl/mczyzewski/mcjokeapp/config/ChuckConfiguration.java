be86a90bf62506d2885e93b4ba6c77419edc346a
Michal Czyzewski <czy.mich@gmail.com>
2020-01-03 15:23:28 +0100

    sort code

--- src/main/java/pl/mczyzewski/mcjokeapp/config/ChuckConfiguration.java
+++ src/main/java/pl/mczyzewski/mcjokeapp/config/ChuckConfiguration.java
@@ -5,8 +5,8 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 //@Configuration
 public class ChuckConfiguration {
 
-//    @Bean
-    public ChuckNorrisQuotes chuckNorrisQuotes(){
+    //    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
 }
--- src/main/java/pl/mczyzewski/mcjokeapp/controlers/JokeController.java
+++ src/main/java/pl/mczyzewski/mcjokeapp/controlers/JokeController.java
@@ -16,10 +16,9 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"/",""})
-    public String showJoke(Model model)
-    {
-        model.addAttribute("joke",jokeService.getJoke());
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
         return "chucknorris";
     }
 }
